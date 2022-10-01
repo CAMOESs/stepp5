48e54d8367727a9fca6b1dea678a7f4db72b16d2
camoes <cam@camoes.fr>
2022-10-01 01:59:16 +0100

    coommit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
     def create
       @task = Task.new(task_params)
       if @task.save
-        redirect_to tasks_path, success: "La tâche a été créée avec succès."
+        redirect_to tasks_path, success: "Task was successfully created."
       else
         render :new
       end
@@ -27,7 +27,7 @@ class TasksController < ApplicationController
   
     def update
       if @task.update(task_params)
-        redirect_to tasks_path, success: "La tâche a été mise à jour avec succès."
+        redirect_to tasks_path, success: "Task was successfully updated."
       else
         render :edit
       end
@@ -35,7 +35,7 @@ class TasksController < ApplicationController
   
     def destroy
       @task.destroy
-      redirect_to tasks_path, danger: "La tâche a été détruite avec succès."
+      redirect_to tasks_path, danger: "Task was successfully destroyed."
     end
   
     private
