b8eb85fe92f0c4640ef80635d4fd17169365f6bb
camoes <cam@camoes.fr>
2022-10-08 10:07:14 +0100

    commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,9 +6,9 @@ class TasksController < ApplicationController
       if params[:sort_deadline_on]
         @tasks = Task.all.order(deadline_on: :asc).page params[:page]
       elsif 
-        if params[:sort_priority] && (conf != 1)
+        if params[:sort_priority] && (conf == 1)
           @tasks = Task.all.order(created_at: :desc).page params[:page]
-        else
+        elsif params[:sort_priority] && (conf != 1)
           @tasks = Task.all.order(priority: :desc).page params[:page]
         end
       else
