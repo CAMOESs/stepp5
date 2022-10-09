b988a1e36b79dd5cdba6f223d54abe95f489b0d9
camoes <cam@camoes.fr>
2022-10-09 18:02:12 +0100

    commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,9 +4,9 @@ class TasksController < ApplicationController
   
     def index
       if params[:sort_deadline_on]
-        @tasks = Task.all.order(deadline_on: :asc).page params[:page]
+        @tasks = Task.deadline_on.page params[:page]
       elsif params[:sort_priority] && (conf != 1)
-        @tasks = Task.all.order(priority: :asc).page params[:page]
+        @tasks = Task.priority.page params[:page]
           
       elsif
         @sess = session[:search]
@@ -18,22 +18,22 @@ class TasksController < ApplicationController
          
           if @title != '' && @status !=''
             if @status == "未着手" && @title != nil
-              @tasks = Task.where("title LIKE ? AND status = 0",@title).page params[:page]
+              @tasks = Task.statuS(@title,0).page params[:page]
             elsif @status == "着手中" && @title != nil
-              @tasks = Task.where("title LIKE ? AND status = 1",@title).page params[:page]
+              @tasks = Task.statuS(@title,1).page params[:page]
             elsif @status == "完了" && @title != nil
-              @tasks = Task.where("title LIKE ? AND status = 2",@title).page params[:page]
+              @tasks = Task.statuS(@title,2).page params[:page]
             end
             
           elsif (@status == '' && @title.is_a?(String))
-            @tasks = Task.where("title LIKE ? ",'%'+@title+'%').page params[:page]
+            @tasks = Task.title(@title).page params[:page]
           elsif @title == '' && @status != nil
             if @status == "未着手" 
-              @tasks = Task.where("status = ?",0).page params[:page]
+              @tasks = Task.status(0).page params[:page]
             elsif @status == "着手中"
-              @tasks = Task.where("status = ?",1).page params[:page]
+              @tasks = Task.status(1).page params[:page]
             elsif @status == "完了"
-              @tasks = Task.where("status = ?",2).page params[:page]
+              @tasks = Task.status(2).page params[:page]
             end
           
           end
--- app/models/task.rb
+++ app/models/task.rb
@@ -7,5 +7,11 @@ class Task < ApplicationRecord
     paginates_per 10
     enum priority: { 高:0, 中:1, 低:2 }
     enum status: {未着手:0, 着手中:1, 完了:2}
+
+    scope :statuS, ->(title,n) { where("title LIKE ? AND status = ?",title,n)}
+    scope :title, ->(title) { where("title LIKE ? ",'%'+title+'%')}
+    scope :status, ->(status) { where("status = ?",status)}
+    scope :deadline_on, -> { all.order(deadline_on: :asc)}
+    scope :priority, -> { all.order(priority: :asc)}
     
 end
