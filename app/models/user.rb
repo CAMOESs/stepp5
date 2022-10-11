e1630e3621d6f1efec5d62043b16aa3c67669517
camoes <cam@camoes.fr>
2022-10-11 05:46:18 +0100

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
         format:{with:/\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/,message:'email error'},
         uniqueness: {case_sensitive: false}
     #validates :password_digest, presence: true, :confirmation =>true
-    validates_confirmation_of :password, if: :password.present?
+    #validates_confirmation_of :password, if: :password.present?
    # validates_presence_of :password_confirmation, if: :password_changed?
     validates_length_of :password_digest, minimum: 6, message: 'passsword long error'
 end
