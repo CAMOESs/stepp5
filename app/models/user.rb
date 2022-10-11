b9849e2cc597b096032da715581e0b091235bd9c
camoes <cam@camoes.fr>
2022-10-11 05:57:42 +0100

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
     attr_accessor :password_confirmation
     has_secure_password
     validates :name, 
-        format:{with:/\A[a-zA-Z0-9_]{3,20}\z/,message:'name error'},
+        format:{with:/\A[a-zA-Z0-9_]{3,20}\z/},
         uniqueness: {case_sensitive: false}
         validates :email, 
-        format:{with:/\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/,message:'email error'},
+        format:{with:/\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/},
         uniqueness: {case_sensitive: false}
     #validates :password_digest, presence: true, :confirmation =>true
     #validates_confirmation_of :password, if: :password.present?
    # validates_presence_of :password_confirmation, if: :password_changed?
-    validates_length_of :password_digest, minimum: 6, message: 'passsword long error'
+    validates_length_of :password_digest, minimum: 6
 end
