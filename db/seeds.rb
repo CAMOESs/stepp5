81420032d96b01bbc0237ebafcfb3075afa3a14c
camoes <cam@camoes.fr>
2022-10-19 11:04:17 +0100

    tasks add

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,24 @@
 
 User.create(name: :hype, email: "hype@gmail.com", password_digest: 123456, admin: true)
 User.create(name: :tom, email: "tom@gmail.com", password_digest: 123456, admin: false)
+10.times do |i|
+    sleep 2
+    Task.create(title: "task#{i+1}", content:"content#{i+1}", deadline_on: Date.new, priority: 2, status: 1, user_id: 1)
+end
+
+10.times do |i|
+    sleep 2
+    Task.create(title: "task#{i+10}", content:"content#{i+10}", deadline_on: Date.new, priority: 0, status: 2, user_id: 1)
+end
+10.times do |i|
+    sleep 2
+    Task.create(title: "task#{i+20}", content:"content#{i+20}", deadline_on: Date.new, priority: 2, status: 0, user_id: 2)
+end
+10.times do |i|
+    sleep 2
+    Task.create(title: "task#{i+30}", content:"content#{i+30}", deadline_on: Date.new, priority: 1, status: 2, user_id: 2)
+end
+10.times do |i|
+    sleep 2
+    Task.create(title: "task#{i+40}", content:"content#{i+40}", deadline_on: Date.new, priority: 0, status: 2, user_id: 1)
+end
